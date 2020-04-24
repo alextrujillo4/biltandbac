7fc8dfc5b7a47a8ddfb432b5a1f85fefc9e845fa
alextrujillo <alexandro4v@gmail.com>
2020-04-24 08:57:03 -0500

    mapas agregados

--- public/src/components/Footer.vue
+++ public/src/components/Footer.vue
@@ -26,8 +26,11 @@
                      x mdi-instagram
                   </v-icon>
               </a>
+
               <div class="mx-2"></div>
+
           </v-row>
+
       </v-col>
         <v-col
                 cols="12"
@@ -35,9 +38,6 @@
                 xl="6"
                 md="4"
                 sm="4">
-            <h1 class="headline ma-4 white--text">
-                Contacto
-            </h1>
             <h1 class="subtitle-2 ma-4 white--text">
                 Oficina en Monterrey
             </h1>
@@ -66,6 +66,16 @@
                      info@bnbseguros.com
                 </a>
             </p>
+            <p class="body-2 white--text">
+                <v-icon color="white" class="mx-4">
+                    mdi-map
+                </v-icon>
+                <a class="white--text" href="https://goo.gl/maps/A1x9j9xeYkQFERoB7">
+                    Rio Manzanares #321 Local 2-A Col. Del Valle San Pedro Garza García, Nuevo León
+                </a>
+            </p>
+            <iframe width="300" height="200" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d563.0476079061549!2d-100.36162409131052!3d25.65644335353391!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8662bdef7d32b73d%3A0x1aa8e1c03fab995c!2sR%C3%ADo%20Manzanares%20321%2C%20Del%20Valle%2C%2066220%20Monterrey%2C%20N.L.!5e0!3m2!1sen!2smx!4v1587736002224!5m2!1sen!2smx"  frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
         </v-col>
         <v-col
                 cols="12"
@@ -94,6 +104,17 @@
                 administracion@bnbseguros.com
                 </a>
             </p>
+
+            <p class="body-2 white--text">
+                <v-icon color="white" class="mx-4">
+                    mdi-map
+                </v-icon>
+                <a class="white--text" href="https://goo.gl/maps/goxmA4c5orGX7TNW6">
+                    Blvd. Venustiano Carranza #4535 Col. Virreyes Residencial Piso 2 Local 16, Saltillo,
+                    Coahuila.
+                </a>
+            </p>
+            <iframe  width="300" height="250" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d900.5562367801009!2d-100.98231687081484!3d25.464163109808286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86886d353eb4f3f7%3A0x37ea7044ff02865a!2sOne%20Sense!5e0!3m2!1sen!2smx!4v1587736216114!5m2!1sen!2smx" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
         </v-col>
     </v-row>
     </v-container>
